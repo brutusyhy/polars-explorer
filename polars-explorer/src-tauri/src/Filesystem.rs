1da4d86e4c68e04ebcf545243e6f5e4468aa7733
Mathews Etiegni <etiegnim@gmail.com>
2024-11-06 21:11:45 +0300

    Update Filesystem.rs
    Added the PathBuf for the directory that aims to make the block less error prone ensuring the path is represented as a proper object

--- polars-explorer/src-tauri/src/Filesystem.rs
+++ polars-explorer/src-tauri/src/Filesystem.rs
@@ -6,6 +6,6 @@ pub fn open_file_dialog() -> Option<PathBuf> {
     // Creates a native file dialog for the user to select a csv file
     FileDialog::new()
         .add_filter("csv", &["csv"])
-        .set_directory("/")
+        .set_directory("PathBuff::from("/"))
         .pick_file()
 }
